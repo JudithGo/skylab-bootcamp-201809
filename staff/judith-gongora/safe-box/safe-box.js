2507758396567dd78151f81703d988c6572d2cd6
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-04 17:37:20 +0200

    safe-box ok

--- staff/judith-gongora/safe-box/safe-box.js
+++ staff/judith-gongora/safe-box/safe-box.js
@@ -1,22 +1,28 @@
-var safeBox = {
 
-    saveSecret: function(secret,password){
-    
-    },
-    getSecret: function(password){
+var safeBox;
+(function(){
+    var _password;
+    var _secret;
+    safeBox = {
+        saveSecret: function(secret, password){
+            if (typeof secret === 'undefined') throw Error ('invalid secret');
+            if (!secret.length) throw Error ('invalid secret');
+            if (!secret.trim().length) throw Error ('invalid secret');
 
+            if (typeof password !== 'string') throw Error ('invalid password');
+            if (!password.length) throw Error ('invalid password');
+            if (!password.trim().length) throw Error ('invalid password');
+            
+            _secret=secret;
+            _password=password;     
+        },
+        retrieveSecret: function(password){
+            if (password!== _password) throw Error ('invalid password');
+            return _secret;
+        }
     }
+})();
 
-}
-
-
-
-
-// safeBox.saveSecret('', 'password');
-
-// var secret = safeBox.getSecret('password')
-
-// console.log(secret)
 
 
 
