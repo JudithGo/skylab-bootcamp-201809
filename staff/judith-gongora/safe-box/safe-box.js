96d458f7bc9abb6b4de8be97f2fc85afece5543f
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-04 14:08:35 +0200

    add safe-box

---
+++ staff/judith-gongora/safe-box/safe-box.js
@@ -0,0 +1,21 @@
+var safeBox = {
+    password: '123',
+    
+    saveSecret: function(secret,password){
+
+    },
+    getSecret: function(password){
+
+    }
+}
+
+
+
+// safeBox.saveSecret('', 'password');
+
+// var secret = safeBox.getSecret('password')
+
+// console.log(secret)
+
+
+
